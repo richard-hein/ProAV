b25f9ad0dc52084513aeb877fc06bb5e53f48ef0
Richard Hein <pyaesone.dev.web@gmail.com>
2025-12-18 13:29:52 +0630

    final touches

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,12 @@ app.use('/api/uploads', uploadRoutes)
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
 
 if (process.env.NODE_ENV === 'production') {
+  // set static folder
   const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
+  // any route that is not api will be redirected to index.html
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
